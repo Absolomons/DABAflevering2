2866e2d171d2f666b9d7624535b623190ec50ed5
Absolomon <andersfs199@gmail.com>
2022-04-06 12:48:57 +0200

    Query til bookingoverview lavet

--- DABAflevering2/DABAflevering2/Program.cs
+++ DABAflevering2/DABAflevering2/Program.cs
@@ -49,6 +49,32 @@ namespace DABAflevering2
                 Console.WriteLine("\n");
             }
 
+
+            var bookingoverviewList = from bookingoverview in dbContext.Set<Bookingoverview>()
+                join room in dbContext.Set<Room>() on bookingoverview.RoomId equals room.RoomId
+                join society in dbContext.Set<Society>() on bookingoverview.Cvr equals society.Cvr
+                join chairman in dbContext.Set<Chairman>() on society.Cvr equals chairman.Cvr
+                select new {bookingoverview, room, society,chairman};
+
+            foreach (var booking in bookingoverviewList)
+            {
+                Console.WriteLine("Society with activity: " + booking.society.Activity + "" +
+                                  " has cvr "
+                                  + booking.society.Cvr
+                                  + " and has chairman "
+                                  + booking.chairman.ChairmanName
+                                  + " has booked room with ID\n "
+                                  + booking.room.RoomId
+                                  + " on address \n"
+                                  + booking.room.RoomAddress
+                                  + " from "
+                                  + booking.bookingoverview.BookingStart
+                                  + " to "
+                                  + booking.bookingoverview.BookingEnd);
+
+                Console.WriteLine("\n");
+            }
+
         }
 
     }
